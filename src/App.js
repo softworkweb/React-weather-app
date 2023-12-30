66b76bc4d1d4ef10fbf69e4840b7c39456e928c8
Egbe Raymond <softworkweb@gmail.com>
2023-12-30 19:32:29 +0100

    adding error messages to fetch errors

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
+// Import necessary dependencies and components
 import { useState } from 'react';
-import Input from './Components/Input';
+import Input from './Components/Input'; // Import the Input component
 import Mobile from './Images/Background-Mobile.jpg';
 import Desktop from './Images/Background-Desktop.jpg';
 import { TbTemperatureCelsius } from 'react-icons/tb';
+
 function App() {
+  // API key for accessing the weather API
   const Api_Key = '83ec3cf470054065b75174902232912';
+
+  // State variables for managing user input and weather data
   const [location, setLocation] = useState('');
-  // const [city, setCity] = useState('');
   const [show, setShow] = useState(false);
   const [temperature, setTemperature] = useState('');
   const [condition, setCondition] = useState('');
-  // const [condition, setCondition] = useState('');
   const [icon, setIcon] = useState('');
-  // const [show, setShow] = useState(false);
+
+  // Function to fetch weather data from the API
   async function getWeather() {
+    // Store the location in local storage
+    localStorage.setItem('location', location);
+    setLocation(''); // Clear the input field
+
+    // Fetch weather data from the WeatherAPI
     const response = await fetch(
       `http://api.weatherapi.com/v1/current.json?key=${Api_Key}&q=${location}`
     );
     const result = await response.json();
+
+    // Update state with the fetched weather data
     setCondition(result.current.condition.text);
     setIcon(`https:${result.current.condition.icon}`);
     setTemperature(result.current.temp_c);
-    setShow(true);
+    console.log(result.current);
+    setShow(true); // Set show to true to display weather information
   }
+
+  // JSX structure for the main component
   return (
     <div className="w-screen text-[#f4f6ff] text-[18px] md:text-[1.1em] lg:text-[1.2em] xl:text-[1.3em]">
+      {/* Display background images based on screen size */}
       <img
         src={Mobile}
         alt=""
@@ -35,22 +50,29 @@ function App() {
         alt=""
         className="hidden md:flex w-full h-screen object-cover"
       />
+
+      {/* Semi-transparent overlay */}
       <div className="bg-[#0e132a9d] absolute inset-0"></div>
+
+      {/* Main content container */}
       <div className="flex flex-col p-4 items-center justify-around absolute inset-0 container mx-auto">
+        {/* Input component for location search */}
         <Input
           location={location}
           setLocation={setLocation}
-          // city={city}
-          // setCity={setCity}
           getWeather={getWeather}
         />
+
+        {/* Display weather information if available */}
         {show ? (
           <>
+            {/* Location header */}
             <h1 className="text-[2em] font-bold ">
-              {location}
-              {/* <span>{city}</span> */}
+              {localStorage.getItem('location')}
             </h1>
+            {/* Display weather icon */}
             <img src={icon} alt="" className="h-[200px]" />
+            {/* Display temperature and condition information */}
             <div className="flex flex-col items-center gap-1">
               <div className="flex">
                 <h3 className="text-[3em] font-bold">{temperature}</h3>
@@ -65,4 +87,5 @@ function App() {
   );
 }
 
+// Export the App component
 export default App;
--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Button from './Button';
-import { TEInput } from 'tw-elements-react';
+import Button from './Button'; // Import the Button component
+import { TEInput } from 'tw-elements-react'; // Import TEInput component from tw-elements-react library
+
+// Functional component for handling user input and form submission
 function Input({ location, setLocation, getWeather }) {
   return (
     <div className="w-full">
+      {/* Form for user input */}
       <form>
+        {/* Input field and submit button aligned in a flex container */}
         <div className="flex flex-col items-center justify-center gap-4 md:flex-row">
+          {/* TEInput component for location input */}
           <TEInput
             className="text-white"
             type="text"
@@ -13,13 +18,8 @@ function Input({ location, setLocation, getWeather }) {
             value={location}
             onChange={(e) => setLocation(e.target.value)}
           ></TEInput>
-          {/* <TEInput
-            className="text-white"
-            type="text"
-            label="City"
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-          ></TEInput> */}
+
+          {/* Button component for triggering weather data retrieval */}
           <Button getWeather={getWeather} />
         </div>
       </form>
@@ -27,4 +27,5 @@ function Input({ location, setLocation, getWeather }) {
   );
 }
 
+// Export the Input component
 export default Input;
